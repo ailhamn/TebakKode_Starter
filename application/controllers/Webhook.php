d5b37d4c8f5892025501ad45affa51834cdbf80a
ailhamn <achmad.ilham28@yahoo.com>
2017-06-04 10:24:33 +0700

    Update Webhook.php

--- application/controllers/Webhook.php
+++ application/controllers/Webhook.php
@@ -1,40 +1,87 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-use \LINE\LINEBot;
-use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
-use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
-use \LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
-use \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
-use \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder;
-use \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder;
+
+
+// SDK for create bot
+
+        use \LINE\LINEBot;
+
+        use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
+
+
+
+// SDK for build message
+
+        use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+
+        use \LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
+
+        use \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
+
+
+
+// SDK for build button and template action
+
+        use \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder;
+
+        use \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder;
+
+
 
 class Webhook extends CI_Controller {
 
-  private $bot;
-  private $events;
-  private $signature; 
-  private $user;
 
-  function __construct()
-  {
-    parent::__construct();
-    $this->load->model('tebakkode_m');
-  }
+    private $events;
 
-  public function index()
-  {
-    if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-      echo "Hello Coders!";
-      header('HTTP/1.1 400 Only POST method allowed');
-      exit;
-    } 
+    private $signature;
 
-    // get request
-    $body = file_get_contents('php://input');
-    $this->signature = isset($_SERVER['HTTP_X_LINE_SIGNATURE']) ? $_SERVER['HTTP_X_LINE_SIGNATURE'] : "-";
-    $this->events = json_decode($body, true);
 
-    // log every event requests);
-    $this->tebakkode_m->log_events($this->signature, $body);
-  }
+    private $bot;
+
+    private $user;
+
+
+    function __construct()
+
+    {
+
+        parent::__construct();
+
+        $this->load->model('tebakkode_m');
+
+    }
+
+
+    public function index()
+
+    {
+
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+
+            echo "Hello Coders!";
+
+            header('HTTP/1.1 400 Only POST method allowed');
+
+            exit;
+
+        }
+
+
+        // get request
+
+        $body = file_get_contents('php://input');
+
+        $this->signature = isset($_SERVER['HTTP_X_LINE_SIGNATURE'])
+
+                ? $_SERVER['HTTP_X_LINE_SIGNATURE']
+
+                : "-";
+
+        $this->events = json_decode($body, true);
+
+
+        $this->tebakkode_m->log_events($this->signature, $body);
+
+    }
+
 }
