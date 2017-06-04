e65a822ae9c087998c194208cfe682361f7f146b
ailhamn <achmad.ilham28@yahoo.com>
2017-06-04 10:25:54 +0700

    Update Tebakkode_m.php

--- application/models/Tebakkode_m.php
+++ application/models/Tebakkode_m.php
@@ -1,18 +1,31 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
+
 class Tebakkode_m extends CI_Model {
 
-  function __construct(){
-    parent::__construct();
-    $this->load->database();
-  }
 
-  function log_events($signature, $body)
-  {
-    $this->db->set('signature', $signature)
-    ->set('events', $body)
-    ->insert('eventlog');
+    function __construct(){
 
-    return $this->db->insert_id();
-  }
+        parent::__construct();
+
+        $this->load->database();
+
+    }
+
+
+    function log_events($signature, $body)
+
+    {
+
+        $this->db->set('signature', $signature)
+
+        ->set('events', $body)
+
+        ->insert('eventlog');
+
+
+        return $this->db->insert_id();
+
+    }
+
 }
